6402f45265fb3675a0316736d5fe4cc83422cc51
Albert Wu <albert12132@gmail.com>
2014-05-21 14:16:19 -0700

    Add markdown reference links

--- markdown.py
+++ markdown.py
@@ -10,6 +10,7 @@ def hash_text(s, label):
 
 def convert(text):
     text, variables = store_vars(text)
+    text, refs = store_references(text)
     text = handle_whitespace(text)
     hashes = {}
     text = hash_blocks(text, hashes)
@@ -18,6 +19,7 @@ def convert(text):
     text = hash_codeblocks(text, hashes)
     text = hash_code(text, hashes)
     text = hash_links(text, hashes)
+    text = hash_ref_links(text, hashes, refs)
     text = hash_tags(text, hashes)
     text = hr_re.sub(hr_sub, text)
     text = emphasis_re.sub(emphasis_sub, text)
@@ -39,7 +41,7 @@ def handle_whitespace(text):
     text = whitespace_re.sub('', text).strip()
     return text
 
-vars_re = re.compile('^~\s+(.*?):\s*(.*?)\n', re.M | re.S)
+vars_re = re.compile('^~\s+(.*?):\s*(.*?)$', re.M)
 def store_vars(text):
     """Extracts variables that can be used in templating engines.
 
@@ -57,6 +59,26 @@ def store_vars(text):
     variables = {var: value for var, value in vars_re.findall(text)}
     text = vars_re.sub('', text)
     return text, variables
+
+references_re = re.compile(r"""
+    \n[ ]{0,3}
+    \[([^\n]+?)\]:
+    [ \t]+
+    (.+?)
+    \s
+    (?:
+        [ \t]*\n?[ \t]*
+        (?P<title>["'])(.*?)(?P=title)
+    )?
+""", re.X | re.S)
+def store_references(text):
+    refs = {}
+    for ref_id, link, _, title in references_re.findall(text):
+        ref_id = re.sub(r'<(.*?)>', r'\1', ref_id).lower().strip()
+        refs[ref_id] = (link, title)
+    text = references_re.sub('', text)
+    return text, refs
+
 
 block_elements = "blockquote|div|form|hr|noscript|ol|p|pre|table"
 block_re = re.compile(r"""
@@ -205,6 +227,35 @@ def hash_links(text, hashes):
         hashes[hashed] = result
         return hashed
     return link_re.sub(link_sub, text)
+
+link_ref_re = re.compile(r"""
+(?<!\\)
+(!?)
+\[(.*?)\]
+\[
+    (.*?)
+\]
+""", re.X | re.S)
+def hash_ref_links(text, hashes, refs):
+    def sub(match):
+        is_img = match.group(1) != ''
+        content = match.group(2)
+        ref = match.group(3).strip().lower()
+        if not ref:
+            ref = content.strip().lower()
+        link, title = refs[ref]
+        if title:
+            title = ' title="{0}"'.format(title)
+        if is_img:
+            result = '<img src="{0}" alt="{1}"{2}>'.format(
+                    link, content, title)
+        else:
+            result = '<a href="{0}"{2}>{1}</a>'.format(link, content,
+                    title)
+        hashed = hash_text(result, 'link')
+        hashes[hashed] = result
+        return hashed
+    return link_ref_re.sub(sub, text)
 
 tag_re = re.compile(r"""<[\w\s:/'"=]+?>""", re.S)
 def hash_tags(text, hashes):
