f2884f8582b10f86940553a338282115cb64b584
Albert Wu <albert12132@gmail.com>
2014-06-01 00:37:46 -0700

    Sample config.py

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+import re
+
+# Path of the current file -- best not to change this
+FILEPATH = os.path.dirname(os.path.abspath(__file__))
+
+#################
+# Substitutions #
+#################
+
+# Substitutions for the linker
+substitutions = [
+    # Add substitutinos of the form
+    # (regex, sub_function),
+]
+
+
+# Use the following to scrape "headers"
+# header_regex = re.compile(r"")
+# def header_translate(match):
+#     pass
+
+#############
+# Templates #
+#############
+
+# List of directories in which to search for templates
+template_dirs = [
+    FILEPATH,
+    # Add directories that contain templates
+    # os.path.join(FILEPATH, 'example'),
+]
+
+# Variables that can be used in templates
+variables = {
+    # Add variables here, like the following
+    # 'example': 'something here',
+}
