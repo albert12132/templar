77cdb77f0c1bb44defff7424de655d5e99408283
Albert Wu <albert12132@gmail.com>
2014-06-01 00:39:11 -0700

    Add common utilities

---
+++ utils/html.py
@@ -0,0 +1,22 @@
+
+##################
+# Header Regexes #
+##################
+
+header_regex = re.compile(r"""
+    <\s*
+    h([1-6])        # \1 is the header level
+    (?:
+        .*?         # attributes
+        id=(['"])   # \2 is the quote
+        (.*?)       # \3 is the actual id
+        \2          # closing quote
+        .*?         # attributes
+    )?>
+    (.*?)           # \4 is the header title
+    <\s*/\s*
+    h\1             # closing header tag
+    \s*>
+""", re.X)
+def header_translate(match)
+    return match.group(1), match.group(3), match.group(4)
