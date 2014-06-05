fa546c21d87dacdc0747c8393e27c9fe3fc04119
Albert Wu <albert12132@gmail.com>
2014-06-04 23:07:07 -0700

    Add assertMarkdown to tests/utils.py and code tag tests

--- tests/markdown/emphasis_tests.py
+++ tests/markdown/emphasis_tests.py
@@ -1,55 +1,48 @@
 from utils import TemplarTest, main
-from markdown import convert, Markdown
 
 class EmTest(TemplarTest):
-    def setUp(self):
-        super().setUp()
-
     def testBasic(self):
         text = "*This is an em*"
         expect = "<p><em>This is an em</em></p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
         text = "_This is an em_"
         expect = "<p><em>This is an em</em></p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
     def testWhitespace(self):
         text = "This is * not an em *"
         expect = "<p>This is * not an em *</p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
         text = "This is _ not an em _"
         expect = "<p>This is _ not an em _</p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
     def testNewline(self):
         text = "This *is an\nem* here"
         expect = "<p>This <em>is an\nem</em> here</p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
     def testMixedDelimiter(self):
         text = "This *is not an em_ here"
         expect = "<p>This *is not an em_ here</p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
     def testUnderscoreInAsterisk(self):
         text = "This *is an em_ here*"
         expect = "<p>This <em>is an em_ here</em></p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
 class StrongTest(TemplarTest):
-    def setUp(self):
-        super().setUp()
-
     def testBasic(self):
         text = "**This is a strong**"
         expect = "<p><strong>This is a strong</strong></p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
         text = "__This is a strong__"
         expect = "<p><strong>This is a strong</strong></p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
     # TODO Original Markdown does not specify the following behavior
     # def testWhitespace(self):
@@ -64,7 +57,7 @@ class StrongTest(TemplarTest):
     def testNewline(self):
         text = "This **is a\nstrong** here"
         expect = "<p>This <strong>is a\nstrong</strong> here</p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
     # TODO Original Markdown does not specify the following behavior
     # def testMixedDelimiter(self):
@@ -75,40 +68,103 @@ class StrongTest(TemplarTest):
     def testUnderscoreInAsterisk(self):
         text = "This **is a strong__ here**"
         expect = "<p>This <strong>is a strong__ here</strong></p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
     def testAsteriskInStrong(self):
         text = "This **is a strong* here**"
         expect = "<p>This <strong>is a strong* here</strong></p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
 class StrongEmTest(TemplarTest):
-    def setUp(self):
-        super().setUp()
-
     def testBasic(self):
         text = "***This is a strong em***"
         expect = "<p><strong><em>This is a strong em</em></strong></p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
         text = "___This is a strong___"
         expect = "<p><strong><em>This is a strong em</em></strong></p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
     def testNewline(self):
         text = "This ***is a\nstrong*** here"
         expect = "<p>This <strong><em>is a\nstrong</em></strong> here</p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
     def testUnderscoreInAsterisk(self):
         text = "This ***is a strong___ here***"
         expect = "<p>This <strong><em>is a strong___ here</em></strong></p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
 
     def testAsteriskInStrongEm(self):
         text = "This ***is a strong em* here***"
         expect = "<p>This <strong><em>is a strong em* here</em></strong></p>"
-        self.assertEqual(expect, convert(text))
+        self.assertMarkdown(text, expect)
+
+class CodeTest(TemplarTest):
+    def testBasic(self):
+        text = "This is a `code tag`"
+        expect = "<p>This is a <code>code tag</code></p>"
+        self.assertMarkdown(text, expect)
+
+    def testNewline(self):
+        text = "This `is a\ncode tag` here"
+        expect = "<p>This <code>is a\ncode tag</code> here</p>"
+        self.assertMarkdown(text, expect)
+
+    def testPreserveAsterisks(self):
+        text = "This `should *not* be` emphasized"
+        expect = "<p>This <code>should *not* be</code> emphasized</p>"
+        self.assertMarkdown(text, expect)
+
+        text = "This `should **not** be` emphasized"
+        expect = "<p>This <code>should **not** be</code> emphasized</p>"
+        self.assertMarkdown(text, expect)
+
+        text = "This `should *not be` emphasized `*here`"
+        expect = "<p>This <code>should *not be</code> emphasized <code>*here</code></p>"
+        self.assertMarkdown(text, expect)
+
+    def testEscape(self):
+        text = "`AT&T`"
+        expect = "<p><code>AT&amp;T</code></p>"
+        self.assertMarkdown(text, expect)
+
+        text = "`<example>`"
+        expect = "<p><code>&lt;example&gt;</code></p>"
+        self.assertMarkdown(text, expect)
+
+        text = '`"double quote"`'
+        expect = "<p><code>&quot;double quote&quot;</code></p>"
+        self.assertMarkdown(text, expect)
+
+    def testMultipleTicks(self):
+        text = "``Two ticks``"
+        expect = "<p><code>Two ticks</code></p>"
+        self.assertMarkdown(text, expect)
+
+        text = "````Four ticks````"
+        expect = "<p><code>Four ticks</code></p>"
+        self.assertMarkdown(text, expect)
+
+    def testWhitespace(self):
+        text = "` Example text `"
+        expect = "<p><code>Example text</code></p>"
+        self.assertMarkdown(text, expect)
+
+
+    def testInternalTicks(self):
+        text = "``Two ` surrounding``"
+        expect = "<p><code>Two ` surrounding</code></p>"
+        self.assertMarkdown(text, expect)
+
+        text = "`` ` ``"
+        expect = "<p><code>`</code></p>"
+        self.assertMarkdown(text, expect)
+
+        text = "```` ``` ````"
+        expect = "<p><code>```</code></p>"
+        self.assertMarkdown(text, expect)
+
 
 if __name__ == '__main__':
     main()
--- tests/utils.py
+++ tests/utils.py
@@ -1,8 +1,11 @@
-import os
-import unittest
-
 import sys
 sys.path.insert(0, '..')
+
+import os
+import unittest
+import re
+from markdown import convert, Markdown
+
 
 class TemplarTest(unittest.TestCase):
     def setUp(self):
@@ -12,12 +15,26 @@ class TemplarTest(unittest.TestCase):
         pass
 
     @staticmethod
-    def open_data(filename):
+    def openData(filename):
         path = os.path.join(os.path.dirname(__file__), 'test-data')
         if not os.path.exists(os.path.join(path, filename)):
             print('No file', filename, 'found in', path)
         with open(os.path.join(path, filename), 'r') as f:
             return f.read()
+
+    @staticmethod
+    def stripLeadingWhitespace(text):
+        text = text.strip('\n')
+        length = len(re.match('\s*', text).group(0))
+        return '\n'.join(line[length:] for line in text.split('\n'))
+
+    def assertMarkdown(self, markdown, output):
+        markdown = self.stripLeadingWhitespace(markdown)
+        output = self.stripLeadingWhitespace(output)
+        try:
+            self.assertEqual(output, convert(markdown))
+        except AssertionError:
+            raise
 
 def main():
     unittest.main()
