ec80738eed505c550e90ba1f75de4fcb7512f6f8
Albert Wu <albert12132@gmail.com>
2014-06-07 09:40:50 -0700

    Add more codeblock tests

--- tests/markdown/codeblock_tests.py
+++ tests/markdown/codeblock_tests.py
@@ -1,6 +1,6 @@
 from utils import TemplarTest, main
 
-class BlockquoteTest(TemplarTest):
+class CodeblockTest(TemplarTest):
     def testBasic(self):
         text = """
         Text here
@@ -17,6 +17,45 @@ class BlockquoteTest(TemplarTest):
         contents</code></pre>
 
         <p>Close</p>
+        """
+        self.assertMarkdown(text, expect)
+
+    def testNewline(self):
+        text = """
+        Text here
+
+            def hello(world):
+                return world
+
+            print(hello('hi'))
+        """
+        expect = """
+        <p>Text here</p>
+
+        <pre><code>def hello(world):
+            return world
+
+        print(hello('hi'))</code></pre>
+        """
+        self.assertMarkdown(text, expect)
+
+        text = """
+        Text here
+
+            def hello(world):
+                return world
+
+
+
+            print(hello('hi'))
+        """
+        expect = """
+        <p>Text here</p>
+
+        <pre><code>def hello(world):
+            return world
+
+        print(hello('hi'))</code></pre>
         """
         self.assertMarkdown(text, expect)
 
@@ -45,6 +84,93 @@ class BlockquoteTest(TemplarTest):
         """
         self.assertMarkdown(text, expect)
 
+    def preserveEmphasisDelimiters(self):
+        text = """
+        Stuff
+
+            Some *em*s
+            Some **strong**s
+            Some ***strong em***s
+            Some _em_s
+            Some __strong__s
+            Some ___strong em___s
+            Some `code`s
+        """
+        expect = """
+        <p>Stuff</p>
+
+        <pre><code>Some *em*s
+        Some **strong**s
+        Some ***strong em***s
+        Some _em_s
+        Some __strong__s
+        Some ___strong em___s
+        Some `code`s</code></pre>
+        """
+        self.assertMarkdown(text, expect)
+
+        text = """
+        Stuff
+
+            At AT&T Park
+        """
+        expect = """
+        <p>Stuff</p>
+
+        <pre><code>At AT&amp;T Park</code></pre>
+        """
+        self.assertMarkdown(text, expect)
+
+    def preserveHeaderMarkers(self):
+        text = """
+        Stuff
+
+            Setext Header1
+            ==============
+
+            Setext Header2
+            --------------
+
+            # Atx Header 1
+            ## Atx Header 2
+            ### Atx Header 3
+            #### Atx Header 4
+            ##### Atx Header 5
+            ###### Atx Header 6
+        """
+        expect = """
+        <p>Stuff</p>
+
+        <pre><code>Setext Header1
+        ==============
+
+        Setext Header2
+        --------------
+
+        # Atx Header 1
+        ## Atx Header 2
+        ### Atx Header 3
+        #### Atx Header 4
+        ##### Atx Header 5
+        ###### Atx Header 6</code></pre>
+        """
+        self.assertMarkdown(text, expect)
+
+    def preserveBlockquote(self):
+        text = """
+        Stuff
+
+            > blockquote
+            > attempt here
+        """
+        expect = """
+        <p>Stuff</p>
+
+        <pre><code>&gt; blockquote
+        &gt; attempt here</code></pre>
+        """
+        self.assertMarkdown(text, expect)
+
     def testEscape(self):
         text = """
         Stuff
@@ -69,7 +195,6 @@ class BlockquoteTest(TemplarTest):
         <pre><code>At AT&amp;T Park</code></pre>
         """
         self.assertMarkdown(text, expect)
-
 
     def testSeparated(self):
         text = """
