eeac4de855f8a44051bb8102bb478df1015a22e9
Albert Wu <albert12132@gmail.com>
2014-06-08 18:12:04 -0700

    Add option to create config file

--- __main__.py
+++ __main__.py
@@ -46,6 +46,29 @@ def extract_configs(config, configs):
 # Command-line Interface #
 ##########################
 
+def config_cmd_options(parser):
+    parser.add_argument('-p', '--path', type=str,
+                        help="Add config.py to specified path")
+
+def config_main(args, configs=None):
+    if args.path:
+        path = args.path
+    else:
+        path = os.getcwd()
+    path = os.path.join(path, 'config.py')
+    config = os.path.join(os.path.dirname(__file__), 'config.py')
+    with open(config, 'r') as f:
+        template = f.read()
+    if not os.path.exists(path) or \
+            'y' in input('Remove existing config.py? [y/n] ').lower():
+        with open(path, 'w') as f:
+            f.write(template)
+        print('Copied config.py to', path)
+        exit(0)
+    else:
+        print('config.py not copied')
+        exit(1)
+
 def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
@@ -61,6 +84,10 @@ def main():
     compile_parser = subparsers.add_parser('compile')
     compile.cmd_options(compile_parser)
     compile_parser.set_defaults(func=compile.main)
+
+    config_parser = subparsers.add_parser('config')
+    config_cmd_options(config_parser)
+    config_parser.set_defaults(func=config_main)
 
     args = parser.parse_args()
 
