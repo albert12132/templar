9c1dd09d67c6808de1e06e9abc1359c0594003d4
Albert Wu <albert12132@gmail.com>
2014-06-29 00:07:34 -0700

    Add setup.py

---
+++ DESCRIPTION.rst
@@ -0,0 +1,7 @@
+Templar
+=======
+
+Templar is a static templating engine written in Python. Templar
+supports template inheritance as well as content linking. In addition,
+Templar allows users to create custom content tags using Python regular
+expressions.
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+dependencies = [
+]
+
+setup(
+    name='templar',
+    version='1.0.0.dev1',
+    description='A static templating engine written in Python',
+    long_description=long_description,
+    url='https://github.com/albert12132/templar',
+    author='Albert Wu',
+    author_email='albert12132@gmail.com',
+    license='MIT',
+    keywords=['templating', 'static template', 'markdown'],
+    packages=find_packages(exclude=['tests*']),
+    install_requires=dependencies,
+    package_data={
+        'templar': ['config.py'],
+    },
+    entry_points={
+        'console_scripts': [
+            'templar=templar.__main__:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: ',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
+
