5e6b885c185946f73efaa0e2944a664f83b2801c
Joy Jeng <joyyjeng@gmail.com>
2015-11-26 00:39:39 -0800

    Implement Config and ConfigBuilder

--- templar/api/config.py
+++ templar/api/config.py
@@ -6,19 +6,150 @@ Users can use this module with the following import statement:
     from templar.api import config
 """
 
+from templar.api import rules
+
 import importlib.machinery
+import os.path
+
+class ConfigBuilderError(Exception):
+    pass
+
+class ConfigBuilder(object):
+    """Builds a Config for Templar's publishing pipeline.
+
+    A Templar Config consists of the following components:
+    - template_dirs
+    - variables
+    - preprocess_rules
+    - postprocess_rules
+
+    Example usage:
+
+        builder = ConfigBuilder()
+        builder.add_template_dirs(
+            'template/dir1',
+            'template/dir2')
+        builder.add_variable('var1', 'value1')
+        builder.add_variable('var2', 'value2')
+        config = builder.build()
+    """
+    def __init__(self,
+            template_dirs=None,
+            variables=None,
+            preprocess_rules=None,
+            postprocess_rules=None):
+        self._template_dirs = list(template_dirs) if template_dirs else []
+        self._variables = variables.copy() if variables else {}
+        self._preprocess_rules = list(preprocess_rules) if preprocess_rules else []
+        self._postprocess_rules = list(postprocess_rules) if postprocess_rules else []
+
+    def add_template_dirs(self, *template_dirs):
+        for template_dir in template_dirs:
+            if not isinstance(template_dir, str):
+                raise ConfigBuilderError(
+                        'template_dir must be a string, but instead was: ' + repr(template_dir))
+            elif not os.path.isdir(template_dir):
+                raise ConfigBuilderError('template_dir path is not a directory: ' + template_dir)
+        self._template_dirs.extend(template_dirs)
+        return self
+
+    def clear_template_dirs(self):
+        self._template_dirs = []
+        return self
+
+    def add_variable(self, variable, value):
+        if not isinstance(variable, str):
+            raise ConfigBuilderError(
+                    'variable must be a string, but instead was: ' + repr(variable))
+        # TODO: Coerce value into some string (possibly unicode, depending on Jinja)
+        self._variables[variable] = value
+        return self
+
+    def clear_variables(self):
+        self._variables = {}
+        return self
+
+    def add_preprocess_rules(self, *preprocess_rules):
+        for rule in preprocess_rules:
+            if not isinstance(rule, rules.Rule):
+                raise ConfigBuilderError(
+                        'preprocess_rule must be a Rule object, but instead was: ' + repr(rule))
+        self._preprocess_rules.extend(preprocess_rules)
+        return self
+
+    def clear_preprocess_rules(self):
+        self._preprocess_rules = []
+        return self
+
+    def add_postprocess_rules(self, *postprocess_rules):
+        for rule in postprocess_rules:
+            if not isinstance(rule, rules.Rule):
+                raise ConfigBuilderError(
+                        'postprocess_rule must be a Rule object, but instead was: ' + repr(rule))
+        self._postprocess_rules.extend(postprocess_rules)
+        return self
+
+    def clear_postprocess_rules(self):
+        self._postprocess_rules = []
+        return self
+
+    def build(self):
+        return Config(
+                self._template_dirs,
+                self._variables,
+                self._preprocess_rules,
+                self._postprocess_rules)
+
 
 class Config(object):
-    """Contains configurations for Templar's publishing pipeline."""
-    pass
+    """An immutable Templar configuration.
+
+    A Config object should not be created directly. Instead, use ConfigBuilder.
+    """
+    def __init__(self,
+            template_dirs,
+            variables,
+            preprocess_rules,
+            postprocess_rules):
+        self._template_dirs = template_dirs
+        self._variables = variables
+        self._preprocess_rules = preprocess_rules
+        self._postprocess_rules = postprocess_rules
+
+    @property
+    def template_dirs(self):
+        return tuple(self._template_dirs)
+
+    @property
+    def variables(self):
+        return self._variables.copy()
+
+    @property
+    def preprocess_rules(self):
+        return tuple(self._preprocess_rules)
+
+    @property
+    def postprocess_rules(self):
+        return tuple(self._postprocess_rules)
+
+    def to_builder(self):
+        return ConfigBuilder(
+                self._template_dirs,
+                self._variables,
+                self._preprocess_rules,
+                self._postprocess_rules)
+
 
 def import_config(config_path):
     """Import a Config from a given path, relative to the current directory.
 
-    The module specified by the config file must contain a variable called
-    `config` that is assigned to a Config object.
+    The module specified by the config file must contain a variable called `configuration` that is
+    assigned to a Config object.
     """
-    # TODO: check if config_path is a file.
+    if not os.path.isfile(config_path):
+        raise ConfigBuilderError(
+                'Could not find Templar configuration file: ' + config_path)
     loader = importlib.machinery.SourceFileLoader(config_path, config_path)
-    return loader.load_module().config
+    module = loader.load_module()
+    return module.configuration
 
---
+++ tests/api/config_test.py
@@ -0,0 +1,101 @@
+"""Tests template/api/config.py"""
+
+from templar.api.config import ConfigBuilder
+from templar.api.config import ConfigBuilderError
+from templar.api.rules import Rule
+
+import unittest
+import mock
+
+class ConfigBuilderTest(unittest.TestCase):
+    def testEmptyBuilder(self):
+        config = ConfigBuilder().build()
+        self.assertSequenceEqual([], config.template_dirs)
+        self.assertDictEqual({}, config.variables)
+        self.assertSequenceEqual([], config.preprocess_rules)
+        self.assertSequenceEqual([], config.postprocess_rules)
+
+    def testTemplateDirs(self):
+        with mock.patch('os.path.isdir', lambda s: True):
+            builder = ConfigBuilder().add_template_dirs('template/path1', 'template/path2')
+        self.assertSequenceEqual(
+                ['template/path1', 'template/path2'],
+                builder.build().template_dirs)
+
+        builder.clear_template_dirs()
+        self.assertSequenceEqual([], builder.build().template_dirs)
+
+    def testTemplateDirs_preventNonStrings(self):
+        with self.assertRaises(ConfigBuilderError) as cm:
+            ConfigBuilder().add_template_dirs(4)
+        self.assertEqual(
+                'template_dir must be a string, but instead was: 4',
+                str(cm.exception))
+
+    def testTemplateDirs_preventNonExistentPath(self):
+        with mock.patch('os.path.isdir', lambda s: False):
+            with self.assertRaises(ConfigBuilderError) as cm:
+                ConfigBuilder().add_template_dirs('no/such/path')
+        self.assertEqual(
+                'template_dir path is not a directory: no/such/path',
+                str(cm.exception))
+
+    def testVariables(self):
+        builder = ConfigBuilder().add_variable('var1', 'val1').add_variable('var2', 'val2')
+        self.assertDictEqual({'var1': 'val1', 'var2': 'val2'}, builder.build().variables)
+
+        builder.clear_variables()
+        self.assertDictEqual({}, builder.build().variables)
+
+    def testVariables_preventNonStrings(self):
+        with self.assertRaises(ConfigBuilderError) as cm:
+            ConfigBuilder().add_variable(4, 'val1')
+        self.assertEqual('variable must be a string, but instead was: 4', str(cm.exception))
+
+    def testPreprocessRules(self):
+        rule1, rule2 = Rule(), Rule()
+        builder = ConfigBuilder().add_preprocess_rules(rule1, rule2)
+        self.assertSequenceEqual([rule1, rule2], builder.build().preprocess_rules)
+
+        builder.clear_preprocess_rules()
+        self.assertSequenceEqual([], builder.build().preprocess_rules)
+
+    def testPreprocessRules_preventNonRules(self):
+        with self.assertRaises(ConfigBuilderError) as cm:
+            ConfigBuilder().add_preprocess_rules(4)
+        self.assertEqual(
+                'preprocess_rule must be a Rule object, but instead was: 4',
+                str(cm.exception))
+
+    def testPostprocessRules(self):
+        rule1, rule2 = Rule(), Rule()
+        builder = ConfigBuilder().add_postprocess_rules(rule1, rule2)
+        self.assertSequenceEqual([rule1, rule2], builder.build().postprocess_rules)
+
+        builder.clear_postprocess_rules()
+        self.assertSequenceEqual([], builder.build().postprocess_rules)
+
+    def testPostprocessRules_preventNonRules(self):
+        with self.assertRaises(ConfigBuilderError) as cm:
+            ConfigBuilder().add_postprocess_rules(4)
+        self.assertEqual(
+                'postprocess_rule must be a Rule object, but instead was: 4',
+                str(cm.exception))
+
+    def testConfigIsImmutable(self):
+        with mock.patch('os.path.isdir', lambda s: True):
+            builder = ConfigBuilder().add_template_dirs('template/path1', 'template/path2')
+        builder.add_variable('var1', 'val1')
+        config = builder.build()
+
+        # Verify config was constructed correctly.
+        self.assertSequenceEqual(['template/path1', 'template/path2'], config.template_dirs)
+        self.assertDictEqual({'var1': 'val1'}, config.variables)
+
+        new_builder = config.to_builder()
+        new_builder.clear_template_dirs()
+        new_builder.add_variable('var2', 'val2')
+
+        # Verify previously built config was not affected by changes to new_builder.
+        self.assertSequenceEqual(['template/path1', 'template/path2'], config.template_dirs)
+        self.assertDictEqual({'var1': 'val1'}, config.variables)
