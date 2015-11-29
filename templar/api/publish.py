ca410d76b1260b9dcf0f44c4ec024f713316328e
Joy Jeng <joyyjeng@gmail.com>
2015-11-29 00:21:58 -0800

    Add templating using jinja2

--- templar/api/publish.py
+++ templar/api/publish.py
@@ -8,7 +8,9 @@ Users can use this module with the following import statement:
 
 from templar import linker
 
-def publish(config, source=None, template=None):
+import jinja2
+
+def publish(config, source=None, template=None, jinja_env=None):
     """Given a config, performs an end-to-end publishing pipeline:
 
         link -> pre-process -> compile -> post-process -> template
@@ -17,15 +19,19 @@ def publish(config, source=None, template=None):
     effectively has nothing to do; an exception is raised.
 
     PARAMETERS:
-    config   -- Config; a context that includes variables, compiler options, and templater
-                information.
-    source   -- str; path to a source file, relative to the current working directory. If None, the
-                publisher effectively becomes a templating engine.
-    template -- str; path to a Jinja template file. Templar treats the path as relative to the list
-                of template directories in config. If the template cannot be found relative to those
-                directories, Templar finally tries the path relative to the current directory.
+    config    -- Config; a context that includes variables, compiler options, and templater
+                 information.
+    source    -- str; path to a source file, relative to the current working directory. If None, the
+                 publisher effectively becomes a templating engine.
+    template  -- str; path to a Jinja template file. Templar treats the path as relative to the list
+                 of template directories in config. If the template cannot be found relative to
+                 those directories, Templar finally tries the path relative to the current
+                 directory.
 
-                If template is None, the publisher effectively becomes a linker and compiler.
+                 If template is None, the publisher effectively becomes a linker and compiler.
+    jinja_env -- jinja2.Environment; if None, a Jinja2 Environment is created with a
+                 FileSystemLoader that is configured with config.template_dirs. Otherwise, the given
+                 Jinja2 Environment is used to retrieve and render the template.
 
     RETURNS:
     str; the result of the publishing pipeline.
@@ -41,9 +47,12 @@ def publish(config, source=None, template=None):
         variables.update(block_map.get_variables())
 
     if template:
-        result = None # TODO: use jinja
+        if not jinja_env:
+            jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(config.template_dirs))
+        jinja_template = jinja_env.get_template(template)
+        result = jinja_template.render(variables)
     else:
-        # template is None implies content is not None, so variables['blocks'] must exist.
+        # template is None implies source is not None, so variables['blocks'] must exist.
         result = variables['blocks']['all']
     return result
 
